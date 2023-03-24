4cfc7217965cc7bd9da7e699ed8ee54d27107b38
Victor Louisa <victor.louisa@gmail.com>
2023-03-24 19:07:26 +0100

    used PressureGauge in AlarmService

--- src/main/java/dev/louisa/tpms/service/AlarmService.java
+++ src/main/java/dev/louisa/tpms/service/AlarmService.java
@@ -6,15 +6,16 @@ public class AlarmService {
     private final double LowPressureThreshold = 24;
     private final double HighPressureThreshold = 29;
     private final Alarm alarm;
+    private final PressureGauge pressureGauge;
 
     public AlarmService(Alarm alarm) {
+        this.pressureGauge = new PressureGauge(LowPressureThreshold, HighPressureThreshold);
         this.alarm = alarm;
     }
 
     public void check(TireSensor tireSensor) {
-        var psiPressureValue = tireSensor.measureTirePressure();
-        if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue) {
-            alarm.activate();
+        if (!(pressureGauge.measure(tireSensor) == Pressure.OPTIMAL)) {
+            alarm.activate();  
         }
     }
 
